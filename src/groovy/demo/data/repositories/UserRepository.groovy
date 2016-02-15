a31183cf1324140e80536ac12ca26861e4590a27
Jitendra Singh Bisht <jitendra.singh@tothenew.com>
2016-02-15 12:58:51 +0530

    MongoRepository interface added

--- src/groovy/demo/data/repositories/UserRepository.groovy
+++ src/groovy/demo/data/repositories/UserRepository.groovy
@@ -1,10 +1,11 @@
 package demo.data.repositories
 
 import demo.data.domain.User
+import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 
 /**
  * Created by jitendra on 9/2/16.
  */
-interface UserRepository extends PagingAndSortingRepository<User, Long> {
+interface UserRepository extends MongoRepository<User, Long> {
 }
